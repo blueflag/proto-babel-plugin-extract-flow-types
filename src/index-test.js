336bbdad84f91c0f015472e2171cb84e84fccca0
Allan Hortle <allanhortle@gmail.com>
2017-09-07 12:38:08 +1000

    Add tests

--- src/index-test.js
+++ src/index-test.js
@@ -1,8 +1,67 @@
-import React from 'react';
+//@flow
 import test from 'ava';
-import protobabelpluginextractflowtypes from './index';
+const proxyquire = require('proxyquire').noCallThru();
 
-test('protobabelpluginextractflowtypes does the right thing', tt => {
-    tt.is(protobabelpluginextractflowtypes(123), 246, 'protobabelpluginextractflowtypes should add 123');
-    tt.is(protobabelpluginextractflowtypes(), 123, 'protobabelpluginextractflowtypes should return 123 when given no arguments');
+import {spy} from 'sinon';
+
+const copySync = spy();
+
+const BabelPluginExtractFlowTypes = proxyquire('./index', {
+    'fs-extra': {copySync}
+}).default;
+
+const FAKE_PATH = {
+    hub: {
+        file: {
+            opts: {
+                filename: 'src/foo.js'
+            }
+        }
+    }
+};
+
+function getState(comment: ?Object): Object {
+    return {
+        file: {
+            ast: {
+                comments: [comment]
+            }
+        },
+        opts: {
+            outDir: 'bar'
+        }
+    };
+}
+
+test.beforeEach(() => {
+    spy(console, 'log');
+    spy(console, 'error');
+    copySync.reset();
+});
+test.afterEach(() => {
+    console.log.restore();
+    console.error.restore();
+});
+
+test('BabelPluginExtractFlowTypes should return the right object shape', (tt: Object) => {
+    tt.is(typeof BabelPluginExtractFlowTypes, 'function');
+    tt.is(typeof BabelPluginExtractFlowTypes(), 'object');
+    tt.is(typeof BabelPluginExtractFlowTypes().visitor, 'object');
+    tt.is(typeof BabelPluginExtractFlowTypes().visitor.Program, 'object');
+    tt.is(typeof BabelPluginExtractFlowTypes().visitor.Program.enter, 'function');
+});
+
+
+test('Program.enter will do do nothing if there is no flow pragma', (tt: Object) => {
+    const {enter} = BabelPluginExtractFlowTypes().visitor.Program;
+    enter(FAKE_PATH, getState());
+    tt.is(console.log.callCount, 0);
+});
+
+test('Program.enter call copySync with from and to locations', (tt: Object) => {
+    const {enter} = BabelPluginExtractFlowTypes().visitor.Program;
+    enter(FAKE_PATH, getState({value: '@flow'}));
+    tt.is(console.log.callCount, 1);
+    tt.is(copySync.callCount, 1);
+    tt.true(copySync.calledWith('src/foo.js', 'bar/foo.js.flow'));
 });
